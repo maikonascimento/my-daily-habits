71e7cb0f4f0be06c63cc69354c5036fec534bf24
Maiko Nascimento <maiko.nascimento@outlook.com>
2026-03-10 19:50:21 -0400

    Meu primeiro app React

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <>
+      <h1>My Daily Habits</h1>
+      <div className="card">
+        <p>
+          Gerencie seus hábitos diários de forma simples e visual.
+        </p>
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App
